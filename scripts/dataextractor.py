18fccd8d9040f4120bcbc0df304505ea1a994ab2
Dani Arnaiz <daniarnaizg@gmail.com>
2019-06-01 02:31:55 +0200

    Añadido dataset Cara/Sin cara

--- scripts/dataextractor.py
+++ scripts/dataextractor.py
@@ -10,14 +10,14 @@ def requestImg(image_name, url):
     r = requests.get(url)
     i = Image.open(BytesIO(r.content))
     
-    if image_name[0] is 'M':
-        i.save("D:\_TFG\TFG-Amazon-Scraper\datasets\dataset-modelo/Modelo/" + image_name)
+    if image_name[0] is 'C':
+        i.save("D:\_TFG\TFG-Amazon-Scraper\datasets\dataset-cara/Cara/" + image_name)
     else:
-        i.save("D:\_TFG\TFG-Amazon-Scraper\datasets\dataset-modelo/SinModelo/" + image_name)
+        i.save("D:\_TFG\TFG-Amazon-Scraper\datasets\dataset-cara/SinCara/" + image_name)
 
 
 def main():
-    with open("../outputs/1K-Male-Female-Dataturks-MODEL-fixed.json", "r") as f:
+    with open("../outputs/1K-Male-Female-Dataturks-FACE.json", "r") as f:
         data = json.load(f)
 
     for i in range(len(data) - 1):
--- scripts/dataturks_fixer.py
+++ scripts/dataturks_fixer.py
@@ -6,14 +6,14 @@ Script usado para arreglar el JSON generado por dataturks apra su correcto funci
 
 def main():
 
-    with open("../outputs/1K-Male-Female-Dataturks-MODEL.json", "r") as f:
+    with open("../outputs/1K-Male-Female-Dataturks-FACE.json", "r") as f:
         s = f.read()
     res = re.sub("\}\n\{", "},{", s)
     f.close()
     
     final = "[" + res + "]"
 
-    output = open("1K-Male-Female-Dataturks-MODEL-fixed.json","w+")
+    output = open("1K-Male-Female-Dataturks-FACE-fixed.json","w+")
     output.write(final)
     output.close()
 
